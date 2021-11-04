62f92cb7feaa23aedf3a6dbdbeb724683be5d222
gqqnbig <gqqnb2005@gmail.com>
2021-11-04 16:41:15 +0800

    Use another contract

--- .github/workflows/makeNewSaleTest.sh
+++ .github/workflows/makeNewSaleTest.sh
@@ -7,16 +7,13 @@
 export FABRIC_CFG_PATH=$PWD/../config/
 source $GITHUB_WORKSPACE/.github/workflows/as-org1.sh
 
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageStoreCRUDServiceImpl:createStore","Args":["1","Target","Weyburn","false"]}'
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageCashDeskCRUDServiceImpl:createCashDesk","Args":["1","1","false"]}'
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openStore","Args":["1"]}'
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openCashDesk","Args":["1"]}'
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"Demo:increase","Args":[]}'
 
 docker ps
 
 docker stop $(docker ps -n 1 --filter 'name=dev' --format '{{.ID}}')
 
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"ProcessSaleServiceImpl:makeNewSale","Args":[]}'
+pci -C mychannel -n cocome --waitForEvent -c '{"function":"Demo:increase","Args":[]}'
 
 docker ps
 
---
+++ src/main/java/Demo.java
@@ -0,0 +1,30 @@
+import com.owlike.genson.Genson;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import java.util.Objects;
+
+@Contract
+public class Demo implements ContractInterface {
+	static Integer count;
+
+	@Transaction(intent = Transaction.TYPE.SUBMIT)
+	public int increase(final Context ctx) {
+		ChaincodeStub stub = ctx.getStub();
+
+		if (count == null) {
+			String json = stub.getStringState("count");
+			if (json != null && Objects.equals(json, "") == false)
+				count = Integer.parseInt(json);
+			else
+				count = 0;
+		}
+		count++;
+		stub.putStringState("count", String.valueOf(count));
+		return count;
+	}
+
+}
