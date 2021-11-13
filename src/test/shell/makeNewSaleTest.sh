a4666ade92adb5635944df8a228bcfa484c868a8
gqqnbig <gqqnb2005@gmail.com>
2021-11-13 13:29:45 +0800

    Use shunit2 for testing
    - In action, remove errexit option
    - change pci from alias to function
    -- In bash, alias are expanded when they are read. At that time, alias may not have been defined.

--- src/test/shell/makeNewSaleTest.sh
+++ src/test/shell/makeNewSaleTest.sh
@@ -1,22 +1,37 @@
-#!/bin/zsh -e
+#!/bin/bash
 
-./network.sh down
-./network.sh up createChannel
-./network.sh deployCC -ccn cocome -ccp $GITHUB_WORKSPACE -ccl java
+oneTimeSetUp() {
+  ./network.sh down
+  ./network.sh up createChannel
+  ./network.sh deployCC -ccn cocome -ccp $GITHUB_WORKSPACE -ccl java
 
-export FABRIC_CFG_PATH=$PWD/../config/
-source $GITHUB_WORKSPACE/src/test/shell/as-org1.sh
+  export FABRIC_CFG_PATH=$PWD/../config/
+  source $GITHUB_WORKSPACE/src/test/shell/as-org1.sh
+}
 
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageStoreCRUDServiceImpl:createStore","Args":["1","Target","Weyburn","false"]}'
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageCashDeskCRUDServiceImpl:createCashDesk","Args":["1","1","false"]}'
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openStore","Args":["1"]}'
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openCashDesk","Args":["1"]}'
+testMakeNewSale() {
+  if ! pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageStoreCRUDServiceImpl:createStore","Args":["1","Target","Weyburn","false"]}'; then
+    fail
+  fi
+  if ! pci -C mychannel -n cocome --waitForEvent -c '{"function":"ManageCashDeskCRUDServiceImpl:createCashDesk","Args":["1","1","false"]}'; then
+    fail
+  fi
+  if ! pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openStore","Args":["1"]}'; then
+    fail
+  fi
+  if ! pci -C mychannel -n cocome --waitForEvent -c '{"function":"CoCoMESystemImpl:openCashDesk","Args":["1"]}'; then
+    fail
+  fi
 
-docker stop $(docker ps -n 1 --filter 'name=dev' --format '{{.ID}}')
+  docker stop "$(docker ps -n 1 --filter 'name=dev' --format '{{.ID}}')"
 
-pci -C mychannel -n cocome --waitForEvent -c '{"function":"ProcessSaleServiceImpl:makeNewSale","Args":[]}'
+  if ! pci -C mychannel -n cocome --waitForEvent -c '{"function":"ProcessSaleServiceImpl:makeNewSale","Args":[]}'; then
+    fail
+  fi
 
-if pci -C mychannel -n cocome --waitForEvent -c '{"function":"ProcessSaleServiceImpl:makeNewSale","Args":[]}'; then
-  echo 'Second makeNewSale call should fail.' >&2
-  exit 1
-fi
+  if pci -C mychannel -n cocome --waitForEvent -c '{"function":"ProcessSaleServiceImpl:makeNewSale","Args":[]}'; then
+    fail 'Second makeNewSale call should fail.'
+  fi
+}
+
+source shunit2
